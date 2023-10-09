14c5590ae345906a3e09024a8d5c9ea7b2cf770b
Ayannasamal <96683706+Ayannasamal@users.noreply.github.com>
2023-10-09 18:28:05 +0530

    Add files via upload

--- main1.js
+++ main1.js
@@ -33,6 +33,7 @@ function gotPoses(results)
 {
   if(results.length > 0)
   {
+	console.log(results);
     scoreLeftWrist = results[0].pose.keypoints[9].score;
 	rightWristX = results[0].pose.rightWrist.x;
 	rightWristY = results[0].pose.rightWrist.y;
@@ -49,6 +50,28 @@ function draw() {
 	image(video, 0, 0, 600, 500);
     fill("red");
     stroke("red");
+	circle(rightWristX, rightWristY, 20);
+	if(rightWristY>0 && rightWristY<=100){
+		document.getElementById("speed").innerHTML="Speed = 0.5x"
+		song.rate(0.5)
+	}
+	else if(rightWristY>100 && rightWristY<=200){
+		document.getElementById("speed").innerHTML="Speed = 1x"
+		song.rate(1)
+	}
+	else if(rightWristY>200 && rightWristY<=300){
+		document.getElementById("speed").innerHTML="Speed = 1.5x"
+		song.rate(1.5)
+	}
+	else if(rightWristY>300 && rightWristY<=400){
+		document.getElementById("speed").innerHTML="Speed = 2x"
+		song.rate(2)
+	}
+
+	else if(rightWristY>400 && rightWristY<=500){
+		document.getElementById("speed").innerHTML="Speed = 2.5x"
+		song.rate(2.5)
+	}
     if(scoreLeftWrist>0.2){
         circle(leftWristX,leftWristY,20);
         InNumberleftwristY= Number(leftWristY);
